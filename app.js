f9ba8b78a9e58f7a41b762b0c1c2d0ab1c61ad93
Non T. <117605943+Ratanon691@users.noreply.github.com>
2024-07-26 10:18:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+function mergeObjects(...objects) {
+  return objects.reduce((acc, obj) => Object.assign(acc, obj), {})
+}
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+const obj3 = { d: 5 };
+
+console.log(mergeObjects(obj1, obj2, obj3));
+// { a: 1, b: 3, c: 4, d: 5 }
